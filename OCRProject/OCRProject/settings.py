857dfc77c4977c6060d31d1862dd93cc06924d69
cherif <simocherif2001@gmail.com>
2022-11-02 23:19:13 +0100

    app

--- OCRProject/OCRProject/settings.py
+++ OCRProject/OCRProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$*_q--)sob**q#-g8e)l^ev006ux2kk&=cr%%x!6n%$dk9zvhv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
