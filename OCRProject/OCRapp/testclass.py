4a9b0ae11e6673faa9103b1e65e492c5c7424d38
cherif <simocherif2001@gmail.com>
2022-11-07 23:15:49 +0100

    Ca marche

---
+++ OCRProject/OCRapp/testclass.py
@@ -0,0 +1,6 @@
+
+class Tesssst:
+    def __init__(self):
+        self.nom = "cherif"
+        self.age = 21
+        self.master = "SII"
--- OCRProject/OCRapp/views.py
+++ OCRProject/OCRapp/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 from django.template import loader
+from .testclass import Tesssst
+
 
 def index(request):
-    return render(request,'OCRapp/index.html')
+    class_ = Tesssst()
+    reponse = {
+        "cherif" :[1,2,3,4,5,6],
+        "yanis" : "Agdlksdbnmfbndfobnkdf",
+        "cls" : class_
+    }
+    return render(request,'OCRapp/index.html',reponse)
 
 
 
